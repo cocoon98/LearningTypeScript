3344c12c18c05b56bfb2a458b8cba24d24ba5ad3
jiangrr <jrr_1998@163.com>
2021-11-11 17:26:18 +0800

    first commit

---
+++ basicTypes/index.ts
@@ -0,0 +1,8 @@
+let isDone: boolean = false; // boolean
+let decLiteral: number = 6; // number
+let str: string = 'Hello World!'; // string
+let list: number[] = [1, 2, 3]; // Array
+let list2: Array<number> = [1, 2, 3]; //Array
+
+let x: [string, number]; // Tuple, allowed to declare a known array of element's count and type, and their type has no need to be equaled.
+x = ['hello', 10];
